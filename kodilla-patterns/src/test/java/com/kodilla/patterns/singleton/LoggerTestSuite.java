e61572fc7bfacc6d9bf4339043b3e7d62b793522
maciek047 <maciek.047@gmail.com>
2020-12-28 11:49:38 +0100

    zadanie 15.1

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,22 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+    @BeforeClass
+    public static void openSettingsFile() {
+        Logger.getInstance().log("last action logged");
+    }
+
+    @Test
+    public void testGetLastLog() {
+        //Given
+        //When
+        String lastLog = Logger.getInstance().getLastLog();
+        System.out.println("Last log: " + lastLog);
+        //Then
+        Assert.assertEquals("last action logged", lastLog);
+    }
+}
